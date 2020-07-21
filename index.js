c1748bbc61bb0590b05ec9536ffae13760e1d577
Eric C <erix2205@gmail.com>
2020-07-21 14:21:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function nestedTarget(){
   var result = document.getElementById('nested').querySelector('.target')
   return result
 }
+
+function increaseRankBy(n){
+  const main = document.getElementsByClassName('.ranked-list');
+  for (let i=0; i< main.length ; i++){
+    main[i].innerHTML = parseInt(`${main[i].innerHTML}` + 5, 10)
+  }
+}
