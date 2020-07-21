2e87c03a54cdd4b1537246476cad3e858939cdd2
Eric C <erix2205@gmail.com>
2020-07-21 14:08:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function getFirstSelector(selector){
   var result = document.querySelector(selector);
   return result
 }
+
+function nestedTarget(){
+  var result = document.querySelector(nested.target)
+  return result
+}
